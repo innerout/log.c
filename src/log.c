2beeb59881251628b639a63b8a8c93c99526e166
George Xanthakis <kompiouterakias@hotmail.com>
2022-02-06 23:42:27 +0200

    [feat] Add support for warnings on format arguments

--- src/log.c
+++ src/log.c
@@ -141,7 +141,7 @@ static void init_event(log_Event *ev, void *udata) {
 }
 
 
-void log_log(int level, const char *file, int line, const char *fmt, ...) {
+COMPILER_WARNING void log_log(int level, const char *file, int line, const char *fmt, ...) {
   log_Event ev = {
     .fmt   = fmt,
     .file  = file,
